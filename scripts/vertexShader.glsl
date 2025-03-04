255bd12b5a26c3872bf3faf913746f2d575f010a
Daniel Vitor <danielv759153@gmail.com>
2025-03-04 19:34:36 -0300

    refactor: change structure of project

--- scripts/vertexShader.glsl
+++
@@ -1,8 +0,0 @@
-varying vec3 vWorldPosition;
-varying vec3 vNormal;
-
-void main() {
-  vWorldPosition = (modelMatrix * vec4(position, 1.0)).xyz;
-  vNormal = normalize(normalMatrix * normal);
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-}
